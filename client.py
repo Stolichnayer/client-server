909ac02c5eed327488c03fc80a7ecb9760ffbf63
unknown <alexperrakis1@gmail.com>
2019-06-19 23:00:30 +0300

    - Rewritten code, more understandable.

--- client.py
+++ client.py
@@ -1,9 +1,19 @@
 import socket
 
+IP = "127.0.0.1"
+PORT = 8000
+BUFFER_SIZE = 1024
+MESSAGE = "Hello from client!"
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect((socket.gethostname(), 8000))
+s.connect((IP, PORT))
 
-msg = s.recv(1024)
+s.send(MESSAGE.encode("utf-8"))
 
-print(msg.decode("utf-8"))
+data = s.recv(BUFFER_SIZE)
+data = data.decode("utf-8")
+
+s.close()
+
+print(f"\n[Client]: Received data: {data}")
--- server.py
+++ server.py
@@ -1,13 +1,28 @@
 import socket
 
+
+IP = "127.0.0.1"
+PORT = 8000
+BUFFER_SIZE = 20
+MESSAGE = "Welcome to server!"
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind((socket.gethostname(), 8000))
-s.listen(5)
+s.bind((IP, PORT))
+s.listen(1)
+conn, address = s.accept()
+
+print(f"\n[Server]: Connection from {address} has been established!")
 
 while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    data = conn.recv(BUFFER_SIZE)
+    data = data.decode("utf-8")
+    if not data:
+        break
+    print(f"[Server]: Received data: {data} ")
 
+    conn.send(MESSAGE.encode("utf-8"))
+
+conn.close()
 
