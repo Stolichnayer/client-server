9e4e5c9e22cf3d84858e3192ddee829a8142c640
unknown <alexperrakis1@gmail.com>
2019-06-20 00:22:00 +0300

    - Added comments.

--- client.py
+++ client.py
@@ -1,18 +1,26 @@
 import socket
 
+# Variables.
 IP = "127.0.0.1"
 PORT = 8000
 BUFFER_SIZE = 1024
 MESSAGE = "Hello from client!"
 
+# Create socket object (Arguments of socket(): address family, socket type).
+# AF_INET -> IPv4.
+# SOCK_STREAM -> TCP.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Establish a connection to the server, initiate the three-way handshake.
 s.connect((IP, PORT))
 
+# Encode to utf-8 and send the message to server.
 s.send(MESSAGE.encode("utf-8"))
 
+# Receive server's data and decode to utf-8.
 data = s.recv(BUFFER_SIZE).decode("utf-8")
 
-s.close()
-
 print(f"\n[Client]: Received data: {data}")
+
+# Close the connection.
+s.close()
--- server.py
+++ server.py
@@ -1,26 +1,42 @@
 import socket
 
+# Variables.
 IP = "127.0.0.1"
 PORT = 8000
 BUFFER_SIZE = 20
 MESSAGE = "Welcome to server!"
 
+# Create socket object (Arguments of socket(): address family, socket type).
+# AF_INET -> IPv4.
+# SOCK_STREAM -> TCP.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Associate the socket with network and port.
 s.bind((IP, PORT))
+
+# Specify the number of connections.
 s.listen(1)
+
+# Block and wait for an incoming connection.
+# When client connects, returns: 1) Connection's socket object, 2) address
 conn, address = s.accept()
 
 print(f"\n[Server]: Connection from {address} has been established!")
 
+# Infinite loop: loop over blocking calls to conn.recv().
 while True:
+    # Receive client's data and decode to utf-8.
     data = conn.recv(BUFFER_SIZE).decode("utf-8")
-    
+
+    # Break if empty bytes object b'' is returned.
     if not data:
         break
+
     print(f"[Server]: Received data: {data} ")
 
+    # Encode to utf-8 and send the message to client.
     conn.send(MESSAGE.encode("utf-8"))
 
+# Close the connection.
 conn.close()
 
